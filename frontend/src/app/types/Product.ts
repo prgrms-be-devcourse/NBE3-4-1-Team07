3048a5b7d07cd6c94d8507485cd1f054369938f6
ByeongGyu Jeon <cube9928@naver.com>
2025-01-16 16:24:51 +0900

    #28 refactor - product 타입 별도로 분리

---
+++ frontend/src/app/types/Product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  quantity: number;
+  imgPath: string;
+}
+
+export interface ProductReponseDto {
+  products: Product[];
+}
