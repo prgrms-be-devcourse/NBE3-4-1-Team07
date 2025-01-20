c47ffe177070949b353b28b02fc04ac44bc58f75
Sapiens <33113480+sapiens2000@users.noreply.github.com>
2025-01-20 12:33:58 +0900

    #60 feat - 관리자 상품 추가 API 구현 (#67)
    * #60 feat - 관리자 상품 추가 API 구현
    
    * #60 feat - 관리자 상품 추가 API 구현

--- frontend/src/app/types/Product.ts
+++ frontend/src/app/types/Product.ts
@@ -11,3 +11,11 @@ export interface ProductResponseDto {
   products: Product[];
 }
 
+export interface ProductRequestDto {
+  name: string;
+  price: number;
+  quantity: number;
+  description: string;
+  imgPath: string;
+}
+
