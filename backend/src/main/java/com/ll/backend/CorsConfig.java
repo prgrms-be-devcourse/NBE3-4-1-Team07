6dfc12902fa5f4aeef4fd94338cc56fefc881864
ByeongGyu Jeon <cube9928@naver.com>
2025-01-21 19:04:04 +0900

    add - cors

---
+++ backend/src/main/java/com/ll/backend/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.ll.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/admin/**")
+//                        .allowedOrigins("http://localhost:3000")  // Next.js 출처
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+}
